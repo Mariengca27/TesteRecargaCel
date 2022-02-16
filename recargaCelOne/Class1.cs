b59e7d7a6dbd76c98bd584d1460f2013149031d7
Mariengca27 <mariengca27>
2022-02-16 14:44:00 -0300

    Commit Unico Recarga Cel Teste

---
+++ recargaCelOne/Class1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+    public static class Class1
+    {
+          public static bool RecargaIsOk(double depMoney)
+        {
+        if (depMoney == 0 || depMoney < 0)
+        {
+
+            return false;
+        }
+        return true;
+    
+        }
+
+    }
+
---
+++ recargaTesteUnidade/UnitTest1.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+
+
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        [Owner("MarianaM")]
+        [Description("Conta positiva o que permite a recarga")]
+        [Priority(1)]
+        public void recargaCelularOk()
+        {
+            double depMoney = 20.0;
+
+            bool resultado = Class1.RecargaIsOk(depMoney);
+
+            Assert.IsTrue(resultado); 
+
+        }
+
+        [TestMethod]
+        [Owner("MarianaM")]
+        [Description("Conta negativa o que não permite a recarga.")]
+        public void recargaCelularNaoOK()
+        {
+
+            double depMoney = -30.0;
+            bool resultado = Class1.RecargaIsOk(depMoney);
+            Assert.IsTrue(resultado);
+        }
+
+
+        [TestMethod]
+        [Owner("MarianaM")]
+        [Description("Conta zerada o que não permite a recarga.")]
+        public void recargaCelularZero()
+        {
+
+            double depMoney = 0.0;
+            bool resultado = Class1.RecargaIsOk(depMoney);
+            Assert.IsTrue(resultado);
+
+        }
+
+    }
+
+
